26fbd7790bd1182a61fb492689aa8f87ea94458e
Americium92 <56517067+Americium92@users.noreply.github.com>
2022-09-29 00:32:14 +0200

    Add files via upload

--- tesssst.py
+++ tesssst.py
@@ -5,31 +5,26 @@ import inspect
 from bs4 import BeautifulSoup
 from datetime import datetime
 import time
+import mysql.connector
+from mysql.connector import Error
 
-#define our URL
-url = 'https://tickets.formula1.com/fr'
-GP = []
-img = "http://toussticks.fr/832-thickbox_default/formula-1-f1-nouveau-logo-monochrome.jpg"
+try:
+    connection = mysql.connector.connect(host='sneakifox.myasustor.com',
+                                         database='root',
+                                         user='Melina1025',
+                                         password='Sneaker')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
 
-#use the session to get the data
-session = HTMLSession()
-response = session.get(url)
-
-#Render the page, up the number on scrolldown to page down multiple times on a page
-response.html.render()
-
-#take the rendered html and find the element that we are interested in
-dates = response.html.find('div.grid-template-2 p.event-date')
-dispo = response.html.find('div.grid-template-2 a.event-link')
-
-for i in range(len(dates)) :
-    print("\n")
-    print(dates[i].text + "\n" + dispo[i].text)
-    print(dispo[i].links)
-    resum = dates[i].text + dispo[i].text
-    if resum in GP :
-                print("")
-    else :
-        GP.append(resum)
-        time.sleep(1)
-        #discord_webhook(resum, info, images_V, "https://tickets.formula1.com" + dispo[i].links)
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
